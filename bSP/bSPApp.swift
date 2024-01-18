8db8cc14352677273c6ac268ee1430c5aed1e067
DanaAlzahrani <dana.nalzahrani@gmail.com>
2024-01-18 09:30:08 +0300

    Initial Commit

---
+++ bSP/bSPApp.swift
@@ -0,0 +1,17 @@
+//
+//  bSPApp.swift
+//  bSP
+//
+//  Created by Developer Dana on 18/01/2024.
+//
+
+import SwiftUI
+
+@main
+struct bSPApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
